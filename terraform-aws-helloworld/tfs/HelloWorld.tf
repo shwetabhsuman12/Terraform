2e5eff43c1032bafa97dd81d8b429bb7981a76e1
shwetabh <shwetabhpoc@gmail.com>
2021-02-16 08:59:04 +0530

    added changes to files

--- terraform-aws-helloworld/tfs/HelloWorld.tf
+++ terraform-aws-helloworld/tfs/HelloWorld.tf
@@ -6,16 +6,15 @@ terraform {
     }
   }
   required_version = ">= 0.14.0"
-  
 }
 
 provider "aws" {
   profile = "default"
-  shared_credentials_file = "./.aws-credentials"
+  shared_credentials_file = "../.aws-credentials"
   region  = "us-west-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "hello-world" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 }
